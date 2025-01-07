fa03936f7f899a23f4dcdc3a3b8e9b676a496201
Frozty-Guy <ajcontreras.ajc@gmail.com>
2025-01-07 13:22:32 -0800

    Loot Items / Quest

--- Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
+++ Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898123afbb222f633bb97bec900a7ad90d02e62d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604da85966af4da3b1ae99fb05d29f6c2e54ad1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
