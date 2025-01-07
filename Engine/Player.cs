60acde4b0e64f9ed64d397a77797b91926f332ee
Frozty-Guy <ajcontreras.ajc@gmail.com>
2025-01-07 13:56:15 -0800

    Loot Items / Quest

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class Player: LivingCreature
+    public class Player : LivingCreature
     {
-        public int Gold {  get; set; }
+        public int Gold { get; set; }
 
         public int ExperiencePoints { get; set; }
 
         public int Level { get; set; }
 
-        public List<InventoryItem> Inventory {  get; set; } 
+        public List<InventoryItem> Inventory { get; set; }
 
         public List<PlayerQuest> Quest { get; set; }
 
-        public Player(int currentHitPoints, int maximumHitPoints, int gold, 
-            int experiencePoints, int level) : base (currentHitPoints, maximumHitPoints)
+        public Player(int currentHitPoints, int maximumHitPoints, int gold,
+            int experiencePoints, int level) : base(currentHitPoints, maximumHitPoints)
         {
             Gold = gold;
             ExperiencePoints = experiencePoints;
